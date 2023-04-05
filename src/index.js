0eea94f1b7ea0dc4ba4ae336d3f7e421ea9aa2a3
zabala <zabalasteven9@gmail.com>
2023-04-05 13:05:11 -0700

    Adding main menu

--- src/index.js
+++ src/index.js
@@ -4,6 +4,27 @@ import Game from "./scripts/game";
 document.addEventListener("DOMContentLoaded", ()=> {
     const canvas = document.getElementById('mister-game');
 
-    let game = new Game(canvas);
+    const divInfo = document.getElementById('div-info');
+    const divButton = document.getElementById('div-button');
+    const startButton = document.createElement('button');
+    startButton.textContent = 'Start!'
+    startButton.setAttribute('id', 'button1');
+    divButton.append(startButton);
+
+    const instructions = document.createElement('button');
+    instructions.textContent = 'Instructions';
+    instructions.setAttribute('id', 'button2');
+    divButton.append(instructions);
+
+    startButton.addEventListener('click', () => {
+        let game = new Game(canvas);
+        divButton.remove();
+        divInfo.remove();
+
+    });
+
+    instructions.addEventListener('click', () => {
+        console.log('done!')
+    });
 });
 
