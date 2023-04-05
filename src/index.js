d2e03bb19779b564a6331f9aa6993714e56cbf2e
zabala <zabalasteven9@gmail.com>
2023-04-05 14:17:24 -0700

    Adding menu for instructions

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import Game from "./scripts/game";
 document.addEventListener("DOMContentLoaded", ()=> {
     const canvas = document.getElementById('mister-game');
 
-    const divInfo = document.getElementById('div-info');
     const divButton = document.getElementById('div-button');
     const startButton = document.createElement('button');
+    const prg = document.createElement('p');
+    prg.textContent = "Created by: Steven Zabala";
+    prg.setAttribute('id', 'info');
     startButton.textContent = 'Start!'
     startButton.setAttribute('id', 'button1');
     divButton.append(startButton);
@@ -15,16 +17,35 @@ document.addEventListener("DOMContentLoaded", ()=> {
     instructions.textContent = 'Instructions';
     instructions.setAttribute('id', 'button2');
     divButton.append(instructions);
+    divButton.append(prg);
 
     startButton.addEventListener('click', () => {
         let game = new Game(canvas);
         divButton.remove();
-        divInfo.remove();
-
     });
 
     instructions.addEventListener('click', () => {
-        console.log('done!')
+        const list = document.getElementById('list');
+
+        const firts = document.createElement('li');
+        firts.textContent = 'use the letters A and D to move to the sides';
+        const second = document.createElement('li');
+        second.textContent = 'user the spacebar to jump';
+
+        list.append(firts);
+        list.append(second);
+
+        const divlist = document.getElementById('instruction-list');
+        const backButton = document.createElement('button');
+        backButton.textContent = 'close';
+        backButton.setAttribute('id', 'button3')
+        divlist.append(backButton);
+
+        backButton.addEventListener('click', () => {
+            list.removeChild(firts);
+            list.removeChild(second);
+            divlist.removeChild(backButton);
+        });
     });
 });
 
